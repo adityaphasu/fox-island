164b544c31f354e6996b2bc1f8c998e3de1e7edf
Aditya Phasu <adityaphasu919@gmail.com>
2023-12-10 20:30:52 +0530

    Add fadein animations to facts

--- src/pages/Facts.jsx
+++ src/pages/Facts.jsx
@@ -1,11 +1,13 @@
 import CTA from "../components/CTA";
 import AnimatedPage from "../components/AnimatedPage";
 import { facts } from "../constants";
-import { pageAnimation } from "../utils/animatedVariants";
+import { fadeIn, fadeInX, pageAnimation } from "../utils/animatedVariants";
+import AnimatedComponent from "../components/AnimatedComponent";
+import { AnimatePresence } from "framer-motion";
 
 const Facts = () => {
   return (
-    <AnimatedPage variants={pageAnimation} className="max-container">
+    <AnimatedPage layout variants={pageAnimation} className="max-container">
       <div className="mt-5 flex flex-col gap-5 lg:text-center">
         <h1 className="head-text lg:text-7xl">
           Let's learn about the World of{" "}
@@ -17,21 +19,24 @@ const Facts = () => {
           these incredible creatures.
         </p>
       </div>
-      {facts.map((fact, index) => (
-        <div
-          key={fact.id}
-          className={`my-16 flex flex-col items-center justify-center gap-8 md:flex-row md:gap-20 
+      <AnimatePresence>
+        {facts.map((fact, index) => (
+          <div
+            key={fact.id}
+            className={`my-16 flex flex-col items-center justify-center gap-8 md:flex-row md:gap-20 
           ${index % 2 === 0 ? "" : "md:flex-row-reverse"}`}>
-          <div className="w-full md:w-3/12 lg:w-2/5">
-            <img src={fact.imageUrl} alt={`Fox Illustration ${index + 1}`} />
+            <AnimatedComponent
+              variants={fadeInX(index % 2 === 0 ? -20 : 20)}
+              className="w-full md:w-3/12 lg:w-2/5">
+              <img src={fact.imageUrl} alt={`Fox Illustration ${index + 1}`} />
+            </AnimatedComponent>
+            <AnimatedComponent variants={fadeIn} className="flex-1">
+              <h2 className="subhead-text">{fact.title}</h2>
+              <p className="mt-2 text-sm text-brown-550">{fact.description}</p>
+            </AnimatedComponent>
           </div>
-          <div className="flex-1">
-            <h2 className="subhead-text">{fact.title}</h2>
-            <p className="mt-2 text-sm text-brown-550">{fact.description}</p>
-          </div>
-        </div>
-      ))}
-
+        ))}
+      </AnimatePresence>
       <hr className="border-orange-300" />
       <CTA />
     </AnimatedPage>
--- src/utils/animatedVariants.js
+++ src/utils/animatedVariants.js
@@ -6,14 +6,21 @@ export const pageAnimation = {
 
 export const fadeIn = {
   initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 },
+  animate: { opacity: 1, transition: { delay: 0.5 } },
 };
 
 export const fadeInUp = {
   initial: { opacity: 0, y: -20 },
   animate: { opacity: 1, y: 0, transition: { delay: 0.5 } },
 };
+
+export const fadeInX = (value) => ({
+  initial: {
+    opacity: 0,
+    x: value,
+  },
+  animate: { opacity: 1, x: 0, transition: { duration: 0.8 } },
+});
 
 export const popOut = {
   initial: {
